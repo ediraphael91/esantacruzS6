4d886091d88bb69eda35a079057eeef24cbda420
ediraphael91 <ediraphael91@gmail.com>
2024-07-14 20:31:02 -0500

    Display alert

--- esantacruzS6/Views/ActEliminar.xaml.cs
+++ esantacruzS6/Views/ActEliminar.xaml.cs
@@ -4,22 +4,85 @@ namespace esantacruzS6.Views;
 
 public partial class ActEliminar : ContentPage
 {
-	public ActEliminar(Estudiante datos)
-	{
-		InitializeComponent();
+    private const string Url = "http://192.168.100.8/semana6/estudiantews.php";
+    private readonly HttpClient client = new HttpClient();
+
+    public ActEliminar(Estudiante datos)
+    {
+        InitializeComponent();
         txtCodigo.Text = datos.codigo.ToString();
         txtNombre.Text = datos.nombre;
         txtApellido.Text = datos.apellido;
         txtEdad.Text = datos.edad.ToString();
     }
 
-    private void btnActualizar_Clicked(object sender, EventArgs e)
+    private async void btnActualizar_Clicked(object sender, EventArgs e)
     {
-		
+        try
+        {
+            int codigo = int.Parse(txtCodigo.Text);
+
+            var datosEstudiante = new Dictionary<string, string>
+         {
+             {"codigo", codigo.ToString()},
+             {"nombre", txtNombre.Text},
+             {"apellido", txtApellido.Text},
+             {"edad",  txtEdad.Text}
+         };
+
+
+            var query = new FormUrlEncodedContent(datosEstudiante);
+            string url = "http://192.168.100.8/semana6/estudiantews.php?" + await query.ReadAsStringAsync();
+
+            HttpClient client = new HttpClient();
+
+            var response = await client.PutAsync(url, null);
+
+            // Verificar la respuesta
+            if (response.IsSuccessStatusCode)
+            {
+                await DisplayAlert("Éxito", "Datos actualizados correctamente", "OK");
+                await Navigation.PushAsync(new vEstudiante());
+            }
+            else
+            {
+                string responseBody = await response.Content.ReadAsStringAsync();
+                await DisplayAlert("Error", $"No se pudo actualizar los datos: {responseBody}", "OK");
+            }
+
+
+        }
+        catch (Exception ex)
+        {
+            await DisplayAlert("Alerta", ex.Message, "OK");
+        }
     }
+
 
     private void btnEliminar_Clicked(object sender, EventArgs e)
     {
+        try
+        {
+            var codigo = txtCodigo.Text;
 
+            var response = client.DeleteAsync($"{Url}?codigo={codigo}").Result;
+            var responseString = response.Content.ReadAsStringAsync().Result;
+
+            if (response.IsSuccessStatusCode)
+            {
+                DisplayAlert("Éxito", "Estudiante eliminado correctamente", "OK");
+                MessagingCenter.Send(this, "ActualizarLista"); // Envía un mensaje para actualizar la lista
+                Navigation.PopAsync();
+            }
+            else
+            {
+                DisplayAlert("Error", $"No se pudo eliminar el estudiante: {responseString}", "OK");
+            }
+        }
+        catch (Exception ex)
+        {
+            DisplayAlert("Alerta", ex.Message, "OK");
+        }
     }
 }
+
--- esantacruzS6/Views/vEstudiante.xaml.cs
+++ esantacruzS6/Views/vEstudiante.xaml.cs
@@ -7,33 +7,39 @@ namespace esantacruzS6.Views;
 
 public partial class vEstudiante : ContentPage
 {
-	private const string Url = "http://192.168.100.8/semana6/estudiantews.php";
-	private readonly HttpClient cliente = new HttpClient();
-	private ObservableCollection<Models.Estudiante> est;
+    private const string Url = "http://192.168.100.8/semana6/estudiantews.php";
+    private readonly HttpClient cliente = new HttpClient();
+    private ObservableCollection<Models.Estudiante> est;
     public vEstudiante()
-	{
-		InitializeComponent();
-		mostrar();
+    {
+        InitializeComponent();
+        //consumir el metodo mostrar
+        mostrar();
 
-	}
+        MessagingCenter.Subscribe<ActEliminar>(this, "ActualizarLista", sender =>
+        {
+            mostrar();
+        });
 
-	public async void mostrar() 
-	{
-		var content = await cliente.GetStringAsync(Url);
-		List<Models.Estudiante> mostrar = JsonConvert.DeserializeObject<List<Models.Estudiante>>(content);
-		est = new ObservableCollection<Models.Estudiante>(mostrar);
-		listaEstudiantes.ItemsSource= est;
-	}
+    }
+
+    public async void mostrar()
+    {
+        var content = await cliente.GetStringAsync(Url);
+        List<Models.Estudiante> mostrar = JsonConvert.DeserializeObject<List<Models.Estudiante>>(content);
+        est = new ObservableCollection<Models.Estudiante>(mostrar);
+        listaEstudiantes.ItemsSource = est;
+    }
 
     private void btnEstudiante_Clicked(object sender, EventArgs e)
     {
-		Navigation.PushAsync(new Views.vAgregar());
+        Navigation.PushAsync(new Views.vAgregar());
     }
 
     private void listaEstudiantes_ItemSelected(object sender, SelectedItemChangedEventArgs e)
     {
-		var objEstudiante = (Estudiante)e.SelectedItem;
-		Navigation.PushAsync(new ActEliminar(objEstudiante));
+        var objEstudiante = (Estudiante)e.SelectedItem;
+        Navigation.PushAsync(new ActEliminar(objEstudiante));
 
     }
 }
